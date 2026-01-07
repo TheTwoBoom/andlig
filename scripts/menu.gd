82915ce36eb37bc89e75230fa7761bdae85fbb30
TheTwoBoom <boom@thetwoboom.xyz>
2026-01-07 20:08:34 +0100

    Boom?

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,8 +1,8 @@
 extends Control
 
 
-func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
+func _on_start_game_button_pressed() -> void:
+	%PlayContainer.visible = true
 
 
 func _on_credits_button_pressed() -> void:
