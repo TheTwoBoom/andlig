564cd6ba1d98886b3963f3910fd577a914da3b42
Trulle <leonard.trulsson@gmail.com>
2026-01-07 17:36:56 +0100

    added killer selection

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,5 +1,12 @@
 extends Control
 
+var killer = null
+
+func _ready() -> void:
+	killer = %Scanner.ALLOWED_INPUTS.pick_random()
+	var label = get_node_or_null("%PanelContainer" + str(killer) + "/Label") as Label
+	var new_theme = preload("res://assets/font/big_font_red.tres")
+	label.theme = new_theme
 
 func _on_scanner_added_to_list() -> void:
 	%AliveLabel.text = "People Alive: " + str(9-%Scanner.dead.size()) + "/9"
