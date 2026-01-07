8fe20c31b8961234f2b64295378ee7261cfc3132
Trulle <leonard.trulsson@gmail.com>
2026-01-07 11:14:23 +0100

    eeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- scanner.gd
+++ scanner.gd
@@ -1,6 +1,6 @@
 extends Node
 
-const ALLOWED_INPUTS = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+const ALLOWED_INPUTS = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 var dead = []
 var last_press = ""
@@ -10,7 +10,7 @@ func _input(event):
 		var keycode = DisplayServer.keyboard_get_keycode_from_physical(event.physical_keycode)
 		var key_name = OS.get_keycode_string(keycode)
 
-		if key_name == "Enter" and not last_press in dead and last_press in ALLOWED_INPUTS:
+		if key_name == "Enter" and not int(last_press) in dead and last_press in ALLOWED_INPUTS:
 			dead.append(int(last_press))
 		
 		last_press = key_name
