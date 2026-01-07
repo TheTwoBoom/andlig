c2392ed11178926b62763c2fcef83e5ae1728829
TheTwoBoom <boom@thetwoboom.xyz>
2026-01-07 20:37:57 +0100

    Commit Message about finally adding some game stuff?

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -11,3 +11,18 @@ func _on_credits_button_pressed() -> void:
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_play_button_pressed() -> void:
+	if PlayerManager.players.size() > 2:
+		get_tree().change_scene_to_file("res://scenes/game.tscn")
+	else:
+		%AddMorePlayers.visible = true
+
+
+func _on_add_player_button_pressed() -> void:
+	PlayerManager.players.append(%LineEdit.text)
+	var label: Label = load("res://assets/menu_player_label.tscn").instantiate()
+	label.text = %LineEdit.text
+	%GridContainer.add_child(label)
+	%LineEdit.text = ""
