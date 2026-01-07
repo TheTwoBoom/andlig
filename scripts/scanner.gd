865a7545c9ec6aa9bac33bf147a23d2a4b2ffc8c
TheTwoBoom <boom@thetwoboom.xyz>
2026-01-07 23:05:41 +0100

    Git Commit Stuff

--- scripts/scanner.gd
+++ scripts/scanner.gd
@@ -13,6 +13,9 @@ func _input(event):
 		var key_name = OS.get_keycode_string(keycode)
 
 		if key_name == "Enter" and not int(last_press) in dead and int(last_press) in ALLOWED_INPUTS:
+			for child in %PlayerGridContainer.get_children():
+				if child.find_child("NameLabel") == PlayerManager.killer and child.find_child("IDLabel"):
+					return
 			dead.append(int(last_press))
 			emit_signal("added_to_list")
 		
