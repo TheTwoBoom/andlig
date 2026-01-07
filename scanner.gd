0924dc29b0a25020d0b546c27089e3a1191a0b01
Trulle <leonard.trulsson@gmail.com>
2026-01-07 10:52:31 +0100

    brokren :'(

--- scanner.gd
+++ scanner.gd
@@ -6,8 +6,10 @@ var last_press = ""
 func _input(event):
 	if event is InputEventKey:
 		var keycode = DisplayServer.keyboard_get_keycode_from_physical(event.physical_keycode)
-		keycode = OS.get_keycode_string(keycode)
 		last_press = keycode
-		if keycode == "Enter" and not last_press in dead :
+	
+		if str(keycode) == "Enter" and not last_press in dead :
 			dead.append(last_press)
 			print(str(dead) + "  " + last_press)
+			
+		keycode = OS.get_keycode_string(keycode)
