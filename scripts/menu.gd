815b19e53d6d34a1726366100bff8a9a1def63fb
Trulle <leonard.trulsson@gmail.com>
2026-01-07 20:50:31 +0100

    added enter for add player input

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,17 +1,13 @@
 extends Control
 
-
 func _on_start_game_button_pressed() -> void:
 	%PlayContainer.visible = true
-
 
 func _on_credits_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/credits.tscn")
 
-
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
-
 
 func _on_play_button_pressed() -> void:
 	if PlayerManager.players.size() > 2:
@@ -19,8 +15,14 @@ func _on_play_button_pressed() -> void:
 	else:
 		%AddMorePlayers.visible = true
 
-
 func _on_add_player_button_pressed() -> void:
+	add_player()
+	
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		add_player()
+	
+func add_player():
 	PlayerManager.players.append(%LineEdit.text)
 	var label: Label = load("res://assets/menu_player_label.tscn").instantiate()
 	label.text = %LineEdit.text
