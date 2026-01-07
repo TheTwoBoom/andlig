299bfd198b6d09e17cc6eaa973f40023ac18141e
Trulle <leonard.trulsson@gmail.com>
2026-01-07 16:22:44 +0100

    added a signal on add

--- scanner.gd
+++ scanner.gd
@@ -5,6 +5,8 @@ const ALLOWED_INPUTS = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 var dead = []
 var last_press = ""
 
+signal added_to_ist
+
 func _input(event):
 	if event is InputEventKey and event.pressed and not event.echo:
 		var keycode = DisplayServer.keyboard_get_keycode_from_physical(event.physical_keycode)
@@ -12,5 +14,6 @@ func _input(event):
 
 		if key_name == "Enter" and not int(last_press) in dead and int(last_press) in ALLOWED_INPUTS:
 			dead.append(int(last_press))
+			emit_signal("added_to_ist")
 		
 		last_press = key_name
