06e9b9f5660ea808435a4e44c3e9dc1237b9e9c0
Trulle <leonard.trulsson@gmail.com>
2026-01-07 10:48:19 +0100

    somethings up

--- scanner.gd
+++ scanner.gd
@@ -1,5 +1,13 @@
 extends Node
 
-func _input(event: InputEvent) -> void:
-	if event.keycode:
-		print(event.keycode)
+var dead = []
+var last_press = ""
+
+func _input(event):
+	if event is InputEventKey:
+		var keycode = DisplayServer.keyboard_get_keycode_from_physical(event.physical_keycode)
+		keycode = OS.get_keycode_string(keycode)
+		last_press = keycode
+		if keycode == "Enter" and not last_press in dead :
+			dead.append(last_press)
+			print(str(dead) + "  " + last_press)
