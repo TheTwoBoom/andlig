a13c81085bdb539eeecf9327d6e6e3dc06100d30
TheTwoBoom <boom@thetwoboom.xyz>
2026-01-08 01:04:17 +0100

    code stuff

--- scripts/scanner.gd
+++ scripts/scanner.gd
@@ -13,10 +13,6 @@ func _input(event):
 		var key_name = OS.get_keycode_string(keycode)
 
 		if key_name == "Enter" and not int(last_press) in dead and int(last_press) in ALLOWED_INPUTS:
-			for child in %PlayerGridContainer.get_children():
-				if child.find_child("NameLabel").text == PlayerManager.killer and child.find_child("IDLabel").text == str(last_press):
-					PlayerManager.reset()
-					get_tree().change_scene_to_file("res://scenes/game_over_killer.tscn")
 			dead.append(int(last_press))
 			emit_signal("added_to_list")
 		
